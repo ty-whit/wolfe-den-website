34a37d26d21e15ad24ebd9cc681711bdb6ed6384
Tyler Whitaker <ty-whit@outlook.com>
2026-06-23 23:33:10 -0400

    ensuring js functions build with page

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = merge(common, {
       patterns: [
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'js', to: 'js', globOptions: { ignore: ['**/app.js'] } },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
